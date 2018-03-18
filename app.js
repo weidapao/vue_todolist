daef8b0279c6be397ea412d2891b15c595462999
Eager <1226393396@qq.com>
2018-03-18 21:28:57 +0800

    增加注册，登录，登出功能

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
 import Vue from 'vue'
+import AV from 'leancloud-storage'
+
+var APP_ID = 'ntPd2VVYTG4fbJ110XHPv2k9-gzGzoHsz';
+var APP_KEY = 'vl5Jc0VGv9BdO86OKbxE8Hn4';
+
+AV.init({
+  appId: APP_ID,
+  appKey: APP_KEY
+});
 
 var app = new Vue({
   el: '#app',
   data: {
+    actionType: 'signUp',
+    formData: {
+      username: '',
+      password: ''
+    },
     newTodo: '',
-    todoList: []
+    todoList: [],
+    currentUser: null,
   },
   created: function(){
     // onbeforeunload文档：https://developer.mozilla.org/zh-CN/docs/Web/API/Window/onbeforeunload
@@ -17,6 +32,8 @@ var app = new Vue({
     let oldData = JSON.parse(oldDataString)
     this.todoList = oldData || []
 
+    this.currentUser = this.getCurrentUser();
+
   },
   methods: {
     addTodo: function(){
@@ -25,12 +42,44 @@ var app = new Vue({
         createdAt: new Date(),
         done: false // 添加一个 done 属性
       })
-      this.newTodo='';
+      this.newTodo = ''
     },
-    // 加了👇这个函数
     removeTodo: function(todo){
       let index = this.todoList.indexOf(todo) // Array.prototype.indexOf 是 ES 5 新加的 API
       this.todoList.splice(index,1) // 不懂 splice？赶紧看 MDN 文档！
+    },
+    signUp: function () {
+      let user = new AV.User();
+      user.setUsername(this.formData.username);
+      user.setPassword(this.formData.password);
+      user.signUp().then((loginedUser) => {
+        this.currentUser = this.getCurrentUser()
+      }, (error) => {
+        alert('注册失败')
+      });
+    },
+    login: function () {
+      AV.User.logIn(this.formData.username, this.formData.password).then((loginedUser) => {
+        this.currentUser = this.getCurrentUser()
+      }, function (error) {
+        alert('登录失败')
+      });
+    },
+    getCurrentUser: function () {
+      let current = AV.User.current()
+      if (current) {
+        let {id, createdAt, attributes: {username}} = current
+        // 上面这句话看不懂就得看 MDN 文档了
+        // 我的《ES 6 新特性列表》里面有链接：https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment
+        return {id, username, createdAt} // 看文档：https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Operators/Object_initializer#ECMAScript_6%E6%96%B0%E6%A0%87%E8%AE%B0
+      } else {
+        return null
+      }
+    },
+    logout: function () {
+      AV.User.logOut()
+      this.currentUser = null
+      window.location.reload()
     }
   }
-})   
+}) 
