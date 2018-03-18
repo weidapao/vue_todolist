307b06f22f1437e125bf483db86818fc0d44ed8a
Eager <1226393396@qq.com>
2018-03-18 20:42:21 +0800

    vue做的todolist

---
+++ app.js
@@ -0,0 +1,36 @@
+import Vue from 'vue'
+
+var app = new Vue({
+  el: '#app',
+  data: {
+    newTodo: '',
+    todoList: []
+  },
+  created: function(){
+    // onbeforeunload文档：https://developer.mozilla.org/zh-CN/docs/Web/API/Window/onbeforeunload
+    window.onbeforeunload = ()=>{
+      let dataString = JSON.stringify(this.todoList) // JSON 文档: https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/JSON
+      window.localStorage.setItem('myTodos', dataString) // 看文档https://developer.mozilla.org/zh-CN/docs/Web/API/Window/localStorage
+    }
+
+    let oldDataString = window.localStorage.getItem('myTodos')
+    let oldData = JSON.parse(oldDataString)
+    this.todoList = oldData || []
+
+  },
+  methods: {
+    addTodo: function(){
+      this.todoList.push({
+        title: this.newTodo,
+        createdAt: new Date(),
+        done: false // 添加一个 done 属性
+      })
+      this.newTodo='';
+    },
+    // 加了👇这个函数
+    removeTodo: function(todo){
+      let index = this.todoList.indexOf(todo) // Array.prototype.indexOf 是 ES 5 新加的 API
+      this.todoList.splice(index,1) // 不懂 splice？赶紧看 MDN 文档！
+    }
+  }
+})   
